c4d35af73aa824894632b0fedded5c68e413f1ad
Rohan <rohan@Rohans-MacBook-Air.local>
2024-11-21 02:12:40 +0530

    4pfkicks

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, template_folder='templets')
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/bootstrap')
+def bootstrap_login():
+	return render_template('bootstrap_login.html')
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+if __name__ == '__main__':
+	app.run(debug=True,port=5050)
